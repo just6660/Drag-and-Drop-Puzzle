16994344552e8ed89445f6b37cf3377c6c35abf7
Justin <just6660@gmail.com>
2022-02-21 12:58:18 -0500

    implemented drag and drop functionality using sortablejs

--- script.js
+++ script.js
@@ -1,210 +1,53 @@
-/*
-var puzzleObject = {};
-const solutionObject = {};
+(async () => {
+  //read in the json files for puzzle and solution
+  const puzzleObject = await fetch("./puzzle.json").then((response) =>
+    response.json()
+  );
+  const solutionObject = await fetch("./solution.json").then((response) =>
+    response.json()
+  );
 
-fetch("puzzle.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    puzzleObject = { ...puzzleObject, ...data };
+  //select the puzzle and solution html sections
+  const puzzleSection = document.querySelector(".puzzle-section");
+  const solutionSection = document.querySelector(".solution-section");
+  //create the puzzle using the data from puzzleObject
+  puzzleObject.lines.forEach((elem, index) => {
+    //create each line
+    const line = document.createElement("div");
+    line.className = "line";
+    line.dataset.indentation = 0;
+    solutionSection.appendChild(line.cloneNode());
+
+    //append tokens to each line
+    puzzleObject.lines[index].tokens.forEach((elem, index1) => {
+      const token = document.createElement("div");
+      token.className = "token draggable";
+      token.setAttribute("draggable", "true");
+      token.dataset.text = elem.text;
+      token.dataset.type = elem.type;
+      token.textContent = elem.text;
+      token.id = `${index}, ${index1}`;
+      line.append(token);
+    });
+    puzzleSection.appendChild(line);
   });
+  const arr = Array.from(puzzleSection.children);
+  const arr1 = Array.from(solutionSection.children);
+  for (let i = 0; i < 4; i++) {
+    new Sortable(arr[i], {
+      group: "nested",
+      group: "shared",
+      animation: 150,
+      fallbackOnBody: true,
+      swapThreshold: 0.9,
+    });
 
-fetch("solution.json")
-  .then((response) => response.json())
-  .then((data) => {
-    Object.assign(solutionObject, data);
-  });
-
-console.log(puzzleObject);
-
-const test = {
-  cars: 1,
-};
-console.log(test);
-*/
-
-const puzzleObject = {
-  lines: [
-    {
-      indentations: 0,
-      tokens: [
-        {
-          text: "(",
-          type: "operator",
-        },
-        {
-          text: "print",
-          type: "built-in-function",
-        },
-        {
-          text: " + ",
-          type: "operator",
-        },
-        {
-          text: ")",
-          type: "operator",
-        },
-        {
-          text: "str({input})",
-          type: "built-in-function",
-        },
-        {
-          text: "{input}",
-        },
-      ],
-    },
-    {
-      indentations: 0,
-      tokens: [
-        {
-          text: " = ",
-          type: "operator",
-        },
-        {
-          text: "{input}",
-        },
-        {
-          text: "count",
-          type: "user-defined-variable",
-        },
-      ],
-    },
-    {
-      indentations: 0,
-      tokens: [
-        {
-          text: "0",
-          type: "number",
-        },
-        {
-          text: " = ",
-          type: "operator",
-        },
-        {
-          text: "count",
-          type: "user-defined-variable",
-        },
-      ],
-    },
-    {
-      indentations: 0,
-      tokens: [
-        {
-          text: "while",
-          type: "keyword",
-        },
-        {
-          text: "{input}",
-        },
-        {
-          text: ":",
-          type: "operator",
-        },
-      ],
-    },
-  ],
-};
-
-const solutionObject = {
-  lines: [
-    {
-      indentations: 0,
-      tokens: [
-        {
-          text: "count",
-          type: "user-defined-variable",
-        },
-        {
-          text: " = ",
-          type: "operator",
-        },
-        {
-          text: "0",
-          type: "number",
-        },
-      ],
-    },
-    {
-      indentations: 0,
-      tokens: [
-        {
-          text: "while",
-          type: "keyword",
-        },
-        {
-          text: "{input}",
-        },
-        {
-          text: ":",
-          type: "operator",
-        },
-      ],
-    },
-    {
-      indentations: 1,
-      tokens: [
-        {
-          text: "print",
-          type: "built-in-function",
-        },
-        {
-          text: "(",
-          type: "operator",
-        },
-        {
-          text: "{input}",
-        },
-        {
-          text: " + ",
-          type: "operator",
-        },
-        {
-          text: "str({input})",
-          type: "built-in-function",
-        },
-        {
-          text: ")",
-          type: "operator",
-        },
-      ],
-    },
-    {
-      indentations: 1,
-      tokens: [
-        {
-          text: "count",
-          type: "user-defined-variable",
-        },
-        {
-          text: " = ",
-          type: "operator",
-        },
-        {
-          text: "{input}",
-        },
-      ],
-    },
-  ],
-};
-
-const puzzleSection = document.querySelector(".puzzle-section");
-
-puzzleObject.lines.forEach((elem, index) => {
-  //create each line
-  const line = document.createElement("div");
-  line.className = "line";
-  line.dataset.indentation = 0;
-
-  //append tokens to each line
-  puzzleObject.lines[index].tokens.forEach((elem) => {
-    const token = document.createElement("div");
-    token.className = "token";
-    token.dataset.text = elem.text;
-    token.dataset.type = elem.type;
-    token.textContent = elem.text;
-    line.append(token);
-  });
-
-  puzzleSection.appendChild(line);
-});
+    new Sortable(arr1[i], {
+      group: "nested",
+      group: "shared",
+      animation: 150,
+      fallbackOnBody: true,
+      swapThreshold: 0.9,
+    });
+  }
+})();
